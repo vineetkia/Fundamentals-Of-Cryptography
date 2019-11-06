c1a74ef38af73be5a9ef15e152c8555d4b7dcacd
Vineet Kumar <44500552+heisenberg-official@users.noreply.github.com>
2019-11-06 11:21:57 +0530

    Create [11]-AES-CFB.py

---
+++ [11]-AES-CFB.py
@@ -0,0 +1,7 @@
+from Crypto.Cipher import AES
+from Crypto import Random
+
+key = b'Sixteen byte key'
+iv = Random.new().read(AES.block_size)
+cipher = AES.new(key, AES.MODE_CFB, iv)
+msg = iv + cipher.encrypt(b'Attack at dawn')
