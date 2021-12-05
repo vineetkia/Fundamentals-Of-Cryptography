5476b871200bfafdb2861ce5978c959470ca2b0e
Vineet Kumar <44500552+vineetkia@users.noreply.github.com>
2021-12-05 17:16:58 +0530

    Update [11]-AES-CFB.py

--- [11]-AES-CFB.py
+++ [11]-AES-CFB.py
@@ -1,7 +1,20 @@
-from Crypto.Cipher import AES
-from Crypto import Random
+from Cryptodome import Random
+from Cryptodome.Cipher import AES 
+from Cryptodome.Util.Padding import pad
+from Cryptodome.Protocol.KDF import PBKDF2
 
-key = b'Sixteen byte key'
-iv = Random.new().read(AES.block_size)
-cipher = AES.new(key, AES.MODE_CFB, iv)
-msg = iv + cipher.encrypt(b'Attack at dawn')
+key = b'BFA32EE58B6F15267694EB56D8648204'
+encryptionData = 'This is a secret message'
+
+data = encryptionData.encode('utf-8')
+cipher_encrypt = AES.new(key, AES.MODE_CFB)
+ciphered_bytes = cipher_encrypt.encrypt(data)
+print(ciphered_bytes)
+
+iv = cipher_encrypt.iv
+ciphered_data = ciphered_bytes
+
+cipher_decrypt = AES.new(key, AES.MODE_CFB, iv=iv)
+deciphered_bytes = cipher_decrypt.decrypt(ciphered_data)
+decrypted_data = deciphered_bytes.decode('utf-8')
+print(decrypted_data)
